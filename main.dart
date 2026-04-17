14f1bc41130be210f8dbf03e222185f97d7e9e08
Zahid Ullah <zahiduullah2@gmail.com>
2026-04-16 21:23:06 -0700

    oop programming

--- main.dart
+++ main.dart
@@ -1,3 +1,30 @@
-void main(){
-  print('Hello Dart Programming:');
+class Car {
+  int _speed;
+  String _model;
+
+  Car(this._model, this._speed);
+
+  // Setter
+  set speed(int value) {
+    if (value >= 0) {    
+      _speed = value;
+    }
+  }
+
+  // Getter
+  int get speed => _speed;
+
+  void display() {
+    print('The speed is: $_speed');
+    print('The model is: $_model');
+  }
+}
+
+void main() {
+  Car c1 = Car("Honda", 2026);
+
+  c1.display();
+
+  c1.speed = 300;   
+  print(c1.speed);   
 }
